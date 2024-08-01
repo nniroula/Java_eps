eeab1dbcb838f03199125be678e416999e1968bf
nabin <nkbhaie2017@gmail.com>
2024-07-31 20:37:23 -0600

    update seed file

--- seed.sql
+++ seed.sql
@@ -5,7 +5,11 @@ use eps_db;
 
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE employee(employee_id INTEGER, name VARCHAR(50), status VARCHAR(50), payRate VARCHAR(25));
+-- table for app version - 3
+-- CREATE TABLE employee(employee_id INTEGER, name VARCHAR(50), status VARCHAR(50), payRate VARCHAR(25));
+
+-- Updated table columns for app version - 4
+CREATE TABLE employee(employee_id INTEGER, name VARCHAR(50), status VARCHAR(50), payRate VARCHAR(25), hours VARCHAR(25), salary VARCHAR(20));
 
 		
 				
